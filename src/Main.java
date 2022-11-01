ab492bf62726ff2f384232fd972cfc259ecc87df
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-11-01 17:08:59 +0300

    третье задание

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Main {
@@ -13,8 +15,20 @@ public class Main {
         System.out.println(productList);
 
 
-       Recipie recipie = new Recipie("Рецепт оливье",Set.of(vinograd,hleb));
+        Recipie recipie = new Recipie("Рецепт оливье", Set.of(vinograd, hleb));
         System.out.println(recipie);
 
+        numberRemove();
+    }
+
+    private static void numberRemove() {
+        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+        numbers.removeIf(next -> next % 2 != 0);
+        System.out.println(numbers);
     }
 }
