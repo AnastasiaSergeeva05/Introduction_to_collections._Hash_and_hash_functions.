4dedd05344f86dd3d64e51073676967c104b967d
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-11-01 13:11:32 +0300

    первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Product vinograd = new Product("Виноград",150.0f,5);
+
+        Product hleb = new Product("Хлеб",20.0f,2);
+        ProductList productList = new ProductList();
+        productList.addProduct(vinograd);
+        productList.addProduct(hleb);
+        System.out.println(productList);
+
+        Product vinograd2 = new Product("Виноград",140.0f,7);
+        productList.addProduct(vinograd2);
+
+
     }
 }
---
+++ src/ProductListException.java
@@ -0,0 +1,11 @@
+public class ProductListException extends RuntimeException {
+
+
+    public ProductListException(String message) {
+        super(message);
+    }
+
+    public ProductListException() {
+
+    }
+}
