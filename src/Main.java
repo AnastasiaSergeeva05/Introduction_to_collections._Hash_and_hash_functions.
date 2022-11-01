6a7c47aede1ba962d956a658dfed7ce4ad3f0610
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-11-01 16:28:01 +0300

    второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
-        Product vinograd = new Product("Виноград",150.0f,5);
+        Product vinograd = new Product("Виноград", 150.0f, 5);
 
-        Product hleb = new Product("Хлеб",20.0f,2);
+        Product hleb = new Product("Хлеб", 20.0f, 2);
+
         ProductList productList = new ProductList();
         productList.addProduct(vinograd);
         productList.addProduct(hleb);
         System.out.println(productList);
 
-        Product vinograd2 = new Product("Виноград",140.0f,7);
-        productList.addProduct(vinograd2);
 
+       Recipie recipie = new Recipie("Рецепт оливье",Set.of(vinograd,hleb));
+        System.out.println(recipie);
 
     }
 }
---
+++ src/RecipeList.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class RecipeList {
+    private final Set<Recipie> recipies = new HashSet<>();
+
+
+    public void addRecipe(Recipie recipie) {
+        if (recipies.contains(recipie)) {
+            throw new IllegalArgumentException("Такой рецепь уже существует");
+        } else {
+            recipies.add(recipie);
+        }
+    }
+}
